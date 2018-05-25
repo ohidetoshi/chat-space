4d0753330de3eaa6c6b7be101daacaaf2c4ee09b
ohidetoshi <onuki_hidetoshi@onukihidetoshinoMacBook-puro.local>
2018-05-25 16:23:31 +0900

    Carrierwave＆メッセージ送信実装

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,6 +2,7 @@ class Message < ApplicationRecord
   belongs_to :group
   belongs_to :user
 
-  validetes :content, presence: true, unless: :image
-  end
+  validates :content, presence: true, unless: :image?
+
+  mount_uploader :image, ImageUploader
 end
